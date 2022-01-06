9f10390569582720b18789e174270eff60dd92aa
Lucivaldo Queiroz <lucivaldo.npqueiroz@gmail.com>
2022-01-05 21:30:09 -0300

    finalizando módulo caracteres

---
+++ caracteres/ProblemaPonto.js
@@ -0,0 +1,4 @@
+const texto = 'Bom\ndia'
+
+console.log(texto.match(/.../gi))
+console.log(texto.match(/..../gi)) //o ponto não engloba o \n
---
+++ caracteres/Unicode.js
@@ -0,0 +1,13 @@
+//no inicio...
+// Um byte (8 bits) - 256
+//Símbolos, pontuação, A-Z, a-z, 0-9
+
+//Dois bytes (16 bits) - 65500+ caracteres
+// +Simbolos, +Pontuação, AZ- a-z, 0-9
+
+//Unicode
+//Quantidade de Bytes Variavel  - Expansivel
+//Suporta mais de 1 milhão caracteres
+//Atualmente tem mais de 100.000 caracteres atribuidos
+
+//https://unicode-table.com/pt
